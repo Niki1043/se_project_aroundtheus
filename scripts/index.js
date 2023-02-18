60ebebd81d6e537ab5813f7ba50f2c1bbcde4005
Nikita Patel <npatel.1043@gmail.com>
2023-02-18 08:27:30 -0800

    Project4Stage4

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,7 @@ const initialCards = [
   },
 ];
 
+//Edit button opens modal
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 
@@ -32,7 +33,51 @@ profileEditButton.addEventListener("click", () => {
   profileEditModal.classList.add("modal_opened");
 });
 
+//Close button closes modal
 const profileModalCloseButton = document.querySelector(".modal__close");
 profileModalCloseButton.addEventListener("click", () => {
   profileEditModal.classList.remove("modal_opened");
+});
+
+//Content from profile populates form placeholders
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+const modalInputNameField = document.querySelector(".modal__title");
+const modalInputDescriptionField = document.querySelector(
+  ".modal__description"
+);
+
+profileEditButton.addEventListener("click", () => {
+  modalInputNameField.value = profileName.textContent;
+  modalInputDescriptionField.value = profileDescription.textContent;
+});
+
+//User Input updates on profile section
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+profileEditForm.addEventListener("submit", (evt) => {
+  console.log("submit");
+  evt.preventDefault();
+  profileName.textContent = modalInputNameField.value;
+  profileDescription.textContent = modalInputDescriptionField.value;
+});
+
+//Render Cards
+const cardListElement = document.querySelector(".cards__list");
+let userCardTemplate = document.querySelector("#card-template").content;
+
+function getCardElement(cardData) {
+  let cardElement = userCardTemplate.querySelector(".card").cloneNode(true);
+  let cardTitle = cardElement.querySelector(".card__name");
+  let cardImage = cardElement.querySelector(".card__image");
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  console.log(cardImage);
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListElement.prepend(cardElement);
 });
